a4f74e6ef89c0f839fe82d13110293512bad274b
CserhatiLilla <lillacserhati2@gmail.com>
2022-04-20 20:19:53 +0200

    created movie.js

---
+++ Frontend/movie.js
@@ -0,0 +1,21 @@
+let title = "Dune"
+
+let length = 155 
+
+let isKidFriendly = false 
+
+let protagonist = {"actorName": "Timothée Chalamet" , "characterName": "Paul Altreides" } 
+
+let antagonist = { "actorName": "Stellan Skarsgård" , "characterName": "Vladimir Harkonnen"}
+        
+let myFavoriteMovie  = {
+    "movies" : [
+    { "sci-fi": "Dune"  } ,
+    { "fantasy": "Lord of the rings" }
+    ] ,
+
+    "favoriteCaracter": [
+        { "Dune": "Paul" } ,
+        { "Lord of the rings": "Legolas"}
+    ]
+}
